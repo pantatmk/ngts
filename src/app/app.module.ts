ec9e6ab87ddf46c2b00454db2341f65e7a27a7c8
dnyanesh-markelytics <dnyanesh@markelytics.com>
2018-08-18 15:46:00 +0530

    first commit angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes , RouterModule } from '@angular/router';
+// import { xdLocalStorage } from 'xdlocalstorage';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { BooksComponent } from './books/books.component';
+
+const appRoutes:Routes = [
+  {path : '', component:PagesComponent},
+  {path : 'pages', component:PagesComponent},
+  {path : 'new-page', component:NewPageComponent},
+  {path : 'books', component:BooksComponent},
+
+];
+// xdLocalStorage.init(
+//   {
+//     iframeUrl:'../src/iframe.html'
+//   }).then(function () {
+//     //an option function to be called once the iframe was loaded and ready for action
+//     console.log('Got iframe ready');
+// });
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    NewPageComponent,
+    BooksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
